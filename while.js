94a5444a2923749465dd9cbeb3fe9fecc5f7b72b
Almantas1112 <aalmantass1112@gmail.com>
2022-04-06 18:16:05 +0300

    Do... While loops test

---
+++ while.js
@@ -0,0 +1,38 @@
+            //First task.
+
+// let skaicius = 0;
+
+// do{
+//     randomNumber = Math.floor(Math.random()*(10-1+1)+1);
+//     skaicius = skaicius + randomNumber;
+//     console.log(randomNumber);
+// }while (skaicius < 100);
+
+            //Second task
+
+// let skaicius = 0;
+
+// do{
+//     randomNumber = Math.floor(Math.random()*(10-1+1)+1);
+//     skaicius = skaicius + randomNumber;
+//     console.log(randomNumber);
+// }while (Number.isInteger(skaicius / 7));
+
+            //Third task.       (not finished)
+
+// var m = 8; //knygos skyriai
+// var skyriaiPerskaityti = 0; //perskaityti skyriai
+// let d = 1; //dienos per kurias perskaitys knyga
+// let s = 0; //perskaito skyriu vidutiniskai per diena
+
+// do{
+//     skyriaiPerskaityti++;
+//     console.log(skyriaiPerskaityti);
+//     if (m >= d){
+//         d = d + skyriaiPerskaityti; 
+//         console.log("Perskaitė per " + skyriaiPerskaityti + " dienas");
+//     }
+// }while (m >= d);
+
+            //4'th task
+
